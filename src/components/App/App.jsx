3399b5297772f5cc7087720fd433745176f542f0
RbAvci <rb.avci@gmail.com>
2024-03-11 22:58:14 +0000

    sort alphabetically by country name

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,14 +3,13 @@ import "./App.css";
 import allCountryScores from "./scores.js";
 
 function App() {
+  allCountryScores.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
   return (
     <>
       <div>
         <h1>High Scores per Country </h1>
         {allCountryScores.map((country, i) => {
-          return (
-            <HighScore key={i} countryName ={country.name} scores={country.scores} />
-          );
+          return <HighScore key={i} countryName={country.name} scores={country.scores} />;
         })}
       </div>
     </>
